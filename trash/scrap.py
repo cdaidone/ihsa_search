b3a443b897edb0c1f22e51e4cf74616f6bc9da5a
cdaidone <cdaidone@ufl.edu>
2016-04-25 15:28:37 -0400

    Major Commit
    File Cleanup

--- trash/scrap.py
+++
@@ -1,6 +0,0 @@
-def list_schools(source):
-    locations = []
-    for row in source:
-        location = row["State"]
-        locations.append(location)
-    return sorted(locations)
--- trash/testapp.py
+++
@@ -1,8 +0,0 @@
-import schools
-from flask import Flask, render_template
-from flask.ext.wtf import Form
-from wtforms import validators, ValidationError
-from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField, SelectField
-from flask.ext.bootstrap import Bootstrap
-
-print(schools)
